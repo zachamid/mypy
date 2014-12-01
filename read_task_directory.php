323c9ec45f6efc7e2d8066b7bff315c19b3f415d
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-01 21:57:22 +0000

    playing with PHP Dir

---
+++ read_task_directory.php
@@ -0,0 +1,23 @@
+<?php
+	function dirTree($dir) {
+    $d = dir($dir);
+    while (false !== ($entry = $d->read())) {
+        if($entry != '.' && $entry != '..' && is_dir($dir.$entry))
+            $arDir[$entry] = dirTree($dir.$entry.'/');
+    }
+    $d->close();
+    return $arDir;
+}
+
+function printTree($array, $level=0) {
+    foreach($array as $key => $value) {
+        echo "<div class='dir' style='width: ".($level*20)."px;'>&nbsp;</div>".$key."<br/>\n";
+        if(is_array($value))
+            printTree($value, $level+1);
+    }
+}
+
+$dir = "~/";
+$arDirTree = dirTree($dir);
+printTree($arDirTree);
+?>
