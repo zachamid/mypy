3121c6f5522a99ace0b7bd8afb2f7489bedc0a23
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-01 23:11:12 +0000

    playing with PHP Dir

--- read_task_directory.php
+++ read_task_directory.php
@@ -1,15 +1,33 @@
 <html>
 <body>
 <?php
-$dir = "../tasks";
-if (is_dir($dir)){
-  if ($dh = opendir($dir)){
-    while (($file = readdir($dh)) !== false){
-      echo "filename:" . $file . "<br>";
-    }
-    closedir($dh);
-  }
-}
+<?php 
+function dirToArray($dir) { 
+   
+   $result = array(); 
+
+   $cdir = scandir($dir); 
+   foreach ($cdir as $key => $value) 
+   { 
+      if (!in_array($value,array(".",".."))) 
+      { 
+         if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) 
+         { 
+            $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value); 
+         } 
+         else 
+         { 
+            $result[] = $value; 
+         } 
+      } 
+   } 
+   
+   return $result; 
+} 
+
+$dir1 = "../tasks";
+$dir_list = dirToArray($dir1);
+print_r($dir_list);
 ?>
 </body>
 </html>
