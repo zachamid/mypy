9950a2352f2f2f338e7c4e0eb21bdae554a7a4d9
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-03 17:22:19 +0000

    Play with PHP Directory

--- read_task_directory.php
+++ read_task_directory.php
@@ -30,46 +30,46 @@ if(!$result = $db->query($sql_query)){
 	die('There was an error running the query [' . $db->error . ']');
 }
 
-file_existance_matrix = array();
+$file_existance_matrix = array();
 $dir = "../tasks";
 while($row = $result->fetch_assoc()){
-	file_existance_matrix_single = array();
+	$file_existance_matrix_single = array();
 	$dir_list = scandir($dir);
 	if(in_array($row['TaskID'], $dir_list))
 	{
-		file_existance_matrix_single['directory']=1;
+		$file_existance_matrix_single['directory']=1;
 		echo $row['Title'].': Directory is there</br>';
 		$dir_list = scandir($dir1.DIRECTORY_SEPARATOR.$row['TaskID']);
 		if(in_array('task_skeleton.py',$dir_list)){
 			echo $row['Title'].': task_skeleton.py present</br>';
-			file_existance_matrix_single['task_skeleton.py']=1;
+			$file_existance_matrix_single['task_skeleton.py']=1;
 		}
 		else{
 			echo $row['Title'].': task_skeleton.py not present</br>';
-			file_existance_matrix_single['task_skeleton.py']=0;
+			$file_existance_matrix_single['task_skeleton.py']=0;
 		}
 		if(in_array('info.xml',$dir_list)){
 			echo $row['Title'].': info.xml present</br>';
-			file_existance_matrix_single['info.xml']=1;
+			$file_existance_matrix_single['info.xml']=1;
 		}
 		else{
 			echo $row['Title'].': info.xml not present</br>';
-			file_existance_matrix_single['info.xml']=0;
+			$file_existance_matrix_single['info.xml']=0;
 		}
 		if(in_array('task_complete.py',$dir_list)){
 			echo $row['Title'].': task_complete.py present</br>';
-			file_existance_matrix_single['task_complete.py']=1;
+			$file_existance_matrix_single['task_complete.py']=1;
 		}
 		else{
 			echo $row['Title'].': task_complete.py not present</br>';
-			file_existance_matrix_single['task_complete.py']=0;
+			$file_existance_matrix_single['task_complete.py']=0;
 		}
 	}
 	else{
 		echo $row['Title'].': Directory is not there</br>';
-		file_existance_matrix_single['directory']=1;
+		$file_existance_matrix_single['directory']=1;
 	}
-	file_existance_matrix[$row['TaskID']] = file_existance_matrix_single;
+	$file_existance_matrix[$row['TaskID']] = file_existance_matrix_single;
 }
 
 echo json_encode file_existance_matrix;
