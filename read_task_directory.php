122c843d4f0939a8e719c6ec49bdc28ad36bf123
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-03 16:32:49 +0000

    Play with Python CGI

--- read_task_directory.php
+++ read_task_directory.php
@@ -24,8 +24,20 @@ function dirToArray($dir) {
    return $result; 
 } 
 
+include 'db_connection.php'
+$sqL_query = "SELECT * FROM Task";
+if(!$result = $db->query($sql_query)){
+	die('There was an error running the query [' . $db->error . ']');
+}
+
 $dir1 = "../tasks";
-$dir_list = dirToArray($dir1);
+$dir_list = scandir($dir1);
+while($row = $result->fetch_assoc()){
+	if(in_array($row['TaskID'], $dir_list)){
+		echo $row['TaskName'].': Directory not there</br>';
+	}
+}
+
 print_r($dir_list);
 ?>
 </body>
