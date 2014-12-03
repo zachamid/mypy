1f37aee8d306718a5b29ccaeeb6567c69861060a
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-03 16:37:34 +0000

    Play with PHP Directory

--- read_task_directory.php
+++ read_task_directory.php
@@ -1,29 +1,6 @@
 <html>
 <body>
 <?php
-/*function dirToArray($dir) { 
-   
-   $result = array(); 
-
-   $cdir = scandir($dir); 
-   foreach ($cdir as $key => $value) 
-   { 
-      if (!in_array($value,array(".",".."))) 
-      { 
-         if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) 
-         { 
-            $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value); 
-         } 
-         else 
-         { 
-            $result[] = $value; 
-         } 
-      } 
-   } 
-   
-   return $result; 
-} */
-
 include 'db_connection.php';
 $sql_query = "SELECT * FROM Task";
 if(!$result = $db->query($sql_query)){
