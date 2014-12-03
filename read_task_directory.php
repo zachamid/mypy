6b4d8203051f82c8cf105358474f5c4030271da2
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-03 18:11:59 +0000

    TaskAdmin.php

--- read_task_directory.php
+++ read_task_directory.php
@@ -1,29 +1,4 @@
-<html>
-<body>
 <?php
-function dirToArray($dir) { 
-   
-   $result = array(); 
-
-   $cdir = scandir($dir); 
-   foreach ($cdir as $key => $value) 
-   { 
-      if (!in_array($value,array(".",".."))) 
-      { 
-         if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) 
-         { 
-            $result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value); 
-         } 
-         else 
-         { 
-            $result[] = $value; 
-         } 
-      } 
-   } 
-   
-   return $result; 
-} 
-
 include 'db_connection.php';
 $sql_query = "SELECT * FROM Task";
 if(!$result = $db->query($sql_query)){
@@ -38,40 +13,30 @@ while($row = $result->fetch_assoc()){
 	if(in_array($row['TaskID'], $dir_list))
 	{
 		$file_existance_matrix[$row['TaskID']]['directory']=1;
-		echo $row['Title'].': Directory is there</br>';
 		$dir_list = scandir($dir1.DIRECTORY_SEPARATOR.$row['TaskID']);
 		if(in_array('task_skeleton.py',$dir_list)){
-			echo $row['Title'].': task_skeleton.py present</br>';
 			$file_existance_matrix[$row['TaskID']]['task_skeleton.py']=1;
 		}
 		else{
-			echo $row['Title'].': task_skeleton.py not present</br>';
 			$file_existance_matrix[$row['TaskID']]['task_skeleton.py']=0;
 		}
 		if(in_array('info.xml',$dir_list)){
-			echo $row['Title'].': info.xml present</br>';
 			$file_existance_matrix[$row['TaskID']]['info.xml']=1;
 		}
 		else{
-			echo $row['Title'].': info.xml not present</br>';
 			$file_existance_matrix[$row['TaskID']]['info.xml']=0;
 		}
 		if(in_array('task_complete.py',$dir_list)){
-			echo $row['Title'].': task_complete.py present</br>';
 			$file_existance_matrix[$row['TaskID']]['task_complete.py']=1;
 		}
 		else{
-			echo $row['Title'].': task_complete.py not present</br>';
 			$file_existance_matrix[$row['TaskID']]['task_complete.py']=0;
 		}
 	}
 	else{
-		echo $row['Title'].': Directory is not there</br>';
 		$file_existance_matrix[$row['TaskID']]['directory']=1;
 	}
 }
 
 echo json_encode($file_existance_matrix);
 ?>
-</body>
-</html>
---
+++ teacher/taskAdmin.php
@@ -0,0 +1,56 @@
+<?php
+	session_start();
+	if(empty($_SESSION['id'])){
+		header('Location: index.php');
+  	}
+  	include '../db_connection.php';
+?>
+<html>
+	<head>
+		<script src="../jquery-1.11.1.min.js"></script>
+    	<script src='../utils.js'></script>
+    	<title>Welcome</title>
+    	<link href="../bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="../general_style.css" rel="stylesheet">
+    	<link href="teacher_style.css" rel="stylesheet">
+    	<script>
+    		function check_directory(){
+    			$.ajax({
+    				url : '/read_task_directory.php'}).done(function(result){
+    					
+    			});
+    		}
+    	</script>
+	</head>
+	<body>
+		<div class="container">
+			<?php
+  				$curr_page='classAdmin.php';
+    			include 'navbar.php';
+    			$sql_query = 'SELECT * FROM Teacher WHERE TeacherID='.$_SESSION['id'];
+    			if(!$result = $db->query($sql_query)){
+    				die('There was an error running the query [' . $db->error . ']');
+  				}
+  				$row = $result->fetch_assoc();
+    		?>
+    		
+    		<div class="container col-sm-6 col-md-9">
+    			<div class="container" style="width:100%">
+    				<div class="panel panel-default translucent">
+      					<h3>Classes Managed</h3>
+      				</div>
+      				<div class="panel panel-default translucent">
+      					<table id="task_list" width="100%" style="border-spacing:10px">
+      						<tr>
+      							<th>TaskID</th>
+      							<th>task_skeleton.py</th>
+      							<th>task_complete.py</th>
+      							<th>info.xml</th>
+      						</tr>
+      					</table>
+      				</div>
+      			</div>
+      		</div>
+    	</div>
+    </body>
+</html> 
