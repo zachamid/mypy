c71906eb5a6532d36e5b31afd38d7831d2a4b0ab
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-06 18:03:11 +0000

    Play with Python

--- cgi-bin/first.py
+++ cgi-bin/first.py
@@ -7,9 +7,25 @@ import MySQLdb
 import MySQLdb.cursors
 cgitb.enable()
 
+posted_data = cgi.FieldStorage()
+cmd = posted_data['cmd']
 db = MySQLdb.connect('localhost','root','S0crat3s34!','test', cursorclass=MySQLdb.cursors.DictCursor);
 cursor = db.cursor()
-cursor.execute("SELECT * FROM Student")
+sql_query=''
+if(cmd == 'Schools'):
+	sql_query = "SELECT DISTINCT School FROM Class"
+elif(cmd == 'Classes'):
+	param = posted_data['param']
+	sql_query = "SELECT * FROM Classes WHERE School='"+param+"'"
+elif(cmd == 'Progress'):
+	param = posted_data['param']
+	sql_query = "SELECT * FROM Progress WHERE StudentID='"+str(param)+"'"
+elif(cmd == 'CheckEmail'):
+	param = posted_data['param']
+	sql_query = "SELECT Email FROM Student WHERE Email='"+param+"' UNION "
+	sql_query = sql_query+"SELECT Email FROM Teacher WHERE Email='"+param+"'"
+
+cursor.execute(sql_query)
 ver = cursor.fetchall()    
 db.close()    
 print """content-type: text/html
