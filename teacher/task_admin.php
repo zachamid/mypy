8eaf188572d061caedbe6551e2deeb87a4c4f058
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-08 12:15:52 +0000

    refactoring

--- teacher/task_admin.php
+++ teacher/task_admin.php
@@ -9,7 +9,7 @@
 	<head>
 		<script src="../jquery-1.11.1.min.js"></script>
     	<script src='../user_functions.js'></script>
-    	<script src='../task_admin_junctions.js'></script>
+    	<script src='../task_admin_functions.js'></script>
     	<title>Welcome</title>
     	<link href="../bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
     	<link href="../general_style.css" rel="stylesheet">
