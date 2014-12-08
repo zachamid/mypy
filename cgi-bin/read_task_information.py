9adaa17af63fbc4fcd4a5a7d180b69318185d384
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-08 18:31:07 +0000

    task administration in Python

--- cgi-bin/read_task_information.py
+++ cgi-bin/read_task_information.py
@@ -3,22 +3,54 @@
 import cgi
 import cgitb
 import os
-#import MySQLdb
-#import MySQLdb.cursors
-#import db_connection
+import json
+import MySQLdb
+import MySQLdb.cursors
+import db_connection
 cgitb.enable()
 
+global path = '../../tasks/'
+
+def get_task_list():
+	db = db_connection.get_connection()
+	cursor = db.cursor()
+	sql = 'SELECT * FROM Task'
+	cursor.execute(sql)
+	out = cursor.fetchall()
+	return out
+	
+
+def retrieve_file_info():
+	tasks = get_task_list()
+	files = os.listdir(path)
+	file_info = dict()
+	for task in tasks:
+		file_info['taskID'] = dict()
+		if task['taskID'] in files:
+			file_info['taskID']['directory'] = 1
+			new_path=path+task['taskID']+"/"
+			task_files=os.listdir(new_path)
+			if 'info.xml' in task_files:
+			 	file_info['taskID']['info.xml'] = 1
+			else:
+			 	file_info['taskID']['info.xml'] = 0
+		else:
+			file_info['taskID']['directory'] = 0
+	return file_info
+	
+	
 #db = db_connection.get_connection()
 #posted_data = cgi.FieldStorage()
+#cmd = posted_data['cmd'].value
 
-path = '../../tasks/'
-files = os.listdir(path)
+
+
 
 print """content-type:text/html
 
 <html><body>
 """
-for file in files:
-	print file+"</br>"
+
+print json.dumps(retrieve_file_list)
 
 print "</body></html>"
