d0d04870f77dba45f5d3dbe1d53a2b7ebbcccdc4
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-14 23:35:56 +0000

    Python Sessions - attempt at index.py

--- common_components.py
+++ common_components.py
@@ -61,3 +61,41 @@ def print_navbar(id, curr_page):
 	</ul><ul class="nav navbar-nav navbar-right">
 	<li><a onclick="clear_cookies()"><span class="glyphicon glyphicon-log-out"></span>&nbsp
 	Log out</a></li></ul></div></div></nav>"""
+	
+def print_navbar_teacher(id, curr_page):
+	sql_query = 'SELECT * FROM Teacher WHERE TeacherID='+id
+	db = db_connection.get_connection()
+	cursor = db.cursor()
+	cursor.execute(sql_query)
+	record = cursor.fetchone()
+	name = record['FirstName']+' '+record['LastName']
+	print """\n
+	<nav class="navbar navbar-default" role="navigation"><div class="container-fluid">
+	<div class="navbar-header">
+	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	<span class="sr-only">Toggle navigation</span><span class="icon-bar"></span>
+	<span class="icon-bar"></span><span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="#">'"""
+	print name
+	print """\n</a>
+	</div><div id="navbar" class="navbar-collapse collapse">
+	<ul class="nav navbar-nav">
+	"""
+	if(curr_page == 'user_page'):
+		print '<li class="active">Details</a></li>'
+	else:
+		print '<li><a href="teacher/user_page.php">Details</a></li>'
+	if(curr_page == 'task_list'):
+		print '<li class="active">Class Administration</a></li>'
+	else:
+		print '<li><a href="user_page.php">Class Administration</a></li>'
+	if(curr_page == 'playground'):
+		print '<li class="active">Task Administration</a></li>'
+	else:
+		print '<li><a href="user_page.php">Task Administration</a></li>'
+	
+	print """\n
+	</ul><ul class="nav navbar-nav navbar-right">
+	<li><a onclick="clear_cookies()"><span class="glyphicon glyphicon-log-out"></span>&nbsp
+	Log out</a></li></ul></div></div></nav>"""
