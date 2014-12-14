576672ebbedb3ae6e91c8d600138ad307ce82e10
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-14 14:27:10 +0000

    Python Sessions

--- cgi-bin/example.py
+++ cgi-bin/example.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python
 
-import sha, time, Cookie, os
+import session, Cookie, cgi, cgitb
 
-cookie = Cookie.SimpleCookie()
+cgitb.enable()
+
 string_cookie = os.environ.get('HTTP_COOKIE')
 
 # If new session
-if not string_cookie:
-   # The sid will be a hash of the server time
-   sid = sha.new(repr(time.time())).hexdigest()
-   # Set the sid in the cookie
-   cookie['sid'] = sid
-   # Will expire in a year
-   cookie['sid']['expires'] = 12 * 30 * 24 * 60 * 60
+if not session.in_session():
+	session.set_session('Student', 0)
 # If already existent session
 else:
-   cookie.load(string_cookie)
-   sid = cookie['sid'].value
+	cookie = session.return_cookie()
+	cookie.load(string_cookie)
 
-print cookie
+id = cookie['id'].value
+type = cookie['type'].value
+session.set_session(type, id+1)
+	
+session.print_cookie()
 print 'Content-Type: text/html\n'
 print '<html><body>'
 
-if string_cookie:
-   print '<p>Already existent session</p>'
-else:
-   print '<p>New session</p>'
+if session.in_session:
+   print type,'</br>',id
 
-print '<p>SID =', sid, '</p>'
 print '</body></html>'
--- cgi-bin/session.py
+++ cgi-bin/session.py
@@ -1,50 +1,34 @@
 #!/usr/bin/python
 
-import shelve
+import Cookie
 import os
 import string
 
-def retrieve_current_session():
-	c = Cookie.SimpleCookie()
-	if 'HTTP_COOKIE' in os.environ:
-		c.load(os.environ.get('HTTP_COOKIE'))
-	return c
+cookie = Cookie.SimpleCookie()
 
-def generate_key():
-	return ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
-
-def create_session(id, type):
-	sessions = shelve.open('shelve.db')
-	c = dict()
-	c['id']=id
-	c['type']=type
-	key = generate_key()
-	sessions[key] = c
-	return 
-
-def get_session():
-	c = retrieve_current_session()
-	current_session=dict()
-	current_session['id'] = c['id']
-	current_session['type'] = c['type']
-	return current_session
-	
-def clear_session():
-	c = retrieve_current_session()
-	c['id'] = ''
-	c['type'] = ''
-	c['id']['expires']='Thu, 01 Jan 1970 00:00:00 GMT'
-	c['type']['expires']='Thu, 01 Jan 1970 00:00:00 GMT'
-
-def is_set():
-	c = retrieve_current_session()
-	if('id' in c and 'type' in c):
-		print 'What?</br>'
-		return 1
-	else:
-		print 'Why?</br>'
+def in_session():
+	cookie_string = os.environ.get('HTTP_COOKIE')
+	if not cookie_string:
 		return 0
+	else
+		cookie.load(cookie_string)
+		if ('id' in cookie and 'type' in cookie):
+			return 1
+		else
+			return 0
+			
+def set_session(type, id):
+	cookie['type'] = type
+	cookie['type']['expires']= 7*24*60*60
+	cookie['id'] = id
+	cookie['id']['expires']= 7*24*60*60
 
-def print_session():
-	c = retrieve_current_session()
-	print c
+def clear_cookies():
+	cookie['type']['expires']='Thu, 01 Jan 1970 00:00:00 GMT'
+	cookie['id']['expires']='Thu, 01 Jan 1970 00:00:00 GMT'
+
+def print_cookie():
+	print cookie
+
+def return_cookie():
+	return cookie
