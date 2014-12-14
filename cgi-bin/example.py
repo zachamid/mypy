7978e3f01f02a9e3646093a1996672ddef538800
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-14 01:42:24 +0000

    Python Sessions

--- cgi-bin/example.py
+++ cgi-bin/example.py
@@ -5,12 +5,12 @@ cookie = Cookie.SimpleCookie()
 string_cookie = os.environ.get('HTTP_COOKIE')
 
 if not string_cookie:
-   sid = sha.new(repr(time.time())).hexdigest()
-   cookie['sid'] = sid
-   message = 'New session'
+   	sid = sha.new(repr(time.time())).hexdigest()
+   	cookie['sid'] = sid
+   	message = 'New session'
 else:
-   cookie.load(string_cookie)
-   sid = cookie['sid'].value
+	cookie.load(string_cookie)
+	sid = cookie['sid'].value
 cookie['sid']['expires'] = 12 * 30 * 24 * 60 * 60
 
 # The shelve module will persist the session data
@@ -21,8 +21,8 @@ session = shelve.open(session_dir + '/sess_' + sid, writeback=True)
 # Retrieve last visit time from the session
 lastvisit = session.get('lastvisit')
 if lastvisit:
-   message = 'Welcome back. Your last visit was at ' + \
-      time.asctime(time.gmtime(float(lastvisit)))
+   	message = 'Welcome back. Your last visit was at ' + \
+      	time.asctime(time.gmtime(float(lastvisit)))
 # Save the current time in the session
 session['lastvisit'] = repr(time.time())
 
