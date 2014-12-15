32a64e0f08f779956fdcee59f8a6622d0afcedb5
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-15 00:24:49 +0000

    Python Sessions - attempt at index.py

--- task_directory_functions.py
+++ task_directory_functions.py
@@ -7,8 +7,16 @@ def retrieve_file_info():
 	db = db_connection.get_connection()
 	cursor = db.cursor()
 	cursor.execute('SELECT * FROM Task')
+	tasks = cursor.fetchall()
 	file_existence_matrix = dict()
-	directories = [x[0] for x in os.walk('../tasks')]
+	dir = '../tasks'
+	directories = [x[1] for x in os.walk(dir)]
+	#for task in tasks:
+	#	if ''.join(dir,'/',task['TaskID']):
+	#		file_existence_matrix[task['TaskID']]['directory'] = 1
+#			files = 
+	#	else:
+	#		file_existence_matrix[task['TaskID']]['directory'] = 0
 	print directories
 
 retrieve_file_info()
