2a5ed0dc15a9e026baf33794b020fdeec3d4c850
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-16 14:01:18 +0000

    rewriting in Python - Test with new System, student section only (update_user.py)

--- update_user.py
+++ update_user.py
@@ -7,14 +7,13 @@ string_cookie = os.environ.get('HTTP_COOKIE')
 cookie = session.return_cookie()
 cookie.load(string_cookie)
 session.print_cookie()
+
+db = db_connection.get_connection()
+posted_data = cgi.FieldStorage()
+id = cookie['id'].value
 print """content-type:text/html
 
 """
-db = db_connection.get_connection()
-posted_data = cgi.FieldStorage(fp=self.rfile,
-    headers=self.headers,
-    environ={'REQUEST_METHOD':'POST'})
-id = cookie['id'].value
 #type = posted_data['type'].value
 #values_to_change = list();
 #if 'FirstName' in posted_data:
