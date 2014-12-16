9b659292a81b1f4354cff1a51721714bbb50b7c9
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-16 13:47:34 +0000

    rewriting in Python - Test with new System, student section only (update_user.py)

--- update_user.py
+++ update_user.py
@@ -14,14 +14,15 @@ db = db_connection.get_connection()
 posted_data = cgi.FieldStorage()
 id = cookie['id'].value
 type = posted_data['type'].value
-values_to_change = list();
-if 'FirstName' in posted_data:
-	values_to_change.append('FirstName="'+posted_data['FirstName']+'"')
-if 'LastName' in posted_data:
-	values_to_change.append('LastName="'+posted_data['LastName']+'"')
-if 'Password' in posted_data:
-	values_to_change.append('Password="'+posted_data['Password']+'"')
+#values_to_change = list();
+#if 'FirstName' in posted_data:
+#	values_to_change.append('FirstName="'+posted_data['FirstName']+'"')
+#if 'LastName' in posted_data:
+#	values_to_change.append('LastName="'+posted_data['LastName']+'"')
+#if 'Password' in posted_data:
+#	values_to_change.append('Password="'+posted_data['Password']+'"')
 
-values_to_change =  ",".join(values_to_change);
+#values_to_change =  ",".join(values_to_change);
 
-sql_statement = 'UPDATE '+type+' SET '+values_to_change+' WHERE '+type+'ID='+id
+#sql_statement = 'UPDATE '+type+' SET '+values_to_change+' WHERE '+type+'ID='+id
+print posted_data
