36df1b5c5d35c46743a9af2b624cc1032f9b600f
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-17 00:06:52 +0000

    rewriting in Python - Test with new System, student section only (update_user fixes)

--- update_user.py
+++ update_user.py
@@ -9,7 +9,7 @@ cookie.load(string_cookie)
 session.print_cookie()
 
 db = db_connection.get_connection()
-posted_data = cgi.FieldStorage()
+posted_data = cgi.FieldStorage(,,environ={'REQUEST_METHOD':'POST'})
 id = cookie['id'].value
 type = cookie['type'].value
 print """content-type:text/html
