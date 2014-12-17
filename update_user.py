1a1401807f13af6300ec2f9c4723c4722bfb8a80
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-17 00:34:49 +0000

    rewriting in Python - Test with new System, student section only (update_user fixes)

--- update_user.py
+++ update_user.py
@@ -4,17 +4,6 @@ import cgi, cgitb, json, MySQLdb, MySQLdb.cursors, db_connection, os
 cgitb.enable()
 
 posted_data = cgi.FieldStorage()
-#string_cookie = os.environ.get('HTTP_COOKIE')
-#cookie = session.return_cookie()
-#cookie.load(string_cookie)
-#session.print_cookie()
-
-db = db_connection.get_connection()
-#id = cookie['id'].value
-#type = cookie['type'].value
-print """content-type:text/html
-
-"""
 values_to_change = list();
 if 'FirstName' in posted_data:
 	values_to_change.append('FirstName="'+posted_data['FirstName'].value+'"')
@@ -24,6 +13,19 @@ if 'Password' in posted_data:
 	values_to_change.append('Password="'+posted_data['Password'].value+'"')
 
 values_to_change =  ",".join(values_to_change);
+
+db = db_connection.get_connection()
+
+
+string_cookie = os.environ.get('HTTP_COOKIE')
+cookie = session.return_cookie()
+cookie.load(string_cookie)
+id = cookie['id'].value
+type = cookie['type'].value
+print """content-type:text/html
+
+"""
+
 
 sql_statement = 'UPDATE '+' SET '+values_to_change+' WHERE '+'ID='
 print sql_statement
