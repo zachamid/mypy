addfdcb18aa6c2874d6cdd2823af13021a85f8b2
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-18 14:57:15 +0000

    testing logout

--- user_functions.js
+++ user_functions.js
@@ -162,14 +162,14 @@ function set_cookies(type_of_user,id){
 }
 
 function clear_cookies(){
-	console.log('Clearing');
+	
 	var cookies = document.cookie.split(";");
-
+	console.log(cookies);
     for (var i = 0; i < cookies.length; i++) {
     	var cookie = cookies[i];
     	var eqPos = cookie.indexOf("=");
     	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
     	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
-    window.location.reload();
+    //window.location.reload();
 }
