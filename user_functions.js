6c7e6c4ab67f45477472cf80e7fdb019bedf74b4
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-18 15:09:35 +0000

    testing logout

--- user_functions.js
+++ user_functions.js
@@ -161,24 +161,14 @@ function set_cookies(type_of_user,id){
 	document.cookie = "id="+id+";path=/";
 }
 
-function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-    else var expires = "";
-    document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function eraseCookie(name) {
-    createCookie(name,"",-1);
-}
-
 function clear_cookies(){
-	console.log('Enter')
-	var cookies = document.cookie.split(";");
-	for (var i = 0; i < cookies.length; i++){
-  		eraseCookie(cookies[i].split("=")[0]);}
+	    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
     //window.location.reload();
 }
