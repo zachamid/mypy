799dfc6007daaf1ce18ec0ec9dab5f0b9fd91d5d
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-19 19:57:47 +0000

    fix for class admin

--- user_functions.js
+++ user_functions.js
@@ -167,8 +167,9 @@ function clear_cookies(){
     for (var i = 0; i < cookies.length; i++) {
     	var cookie = cookies[i];
     	var eqPos = cookie.indexOf("=");
-    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    	console.log(name);
     	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
+    console.log(document.cookie.split(";"));
+    window.location.reload();
+    
 }
