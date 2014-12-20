776e90eeff66c5a82c6220b87aa8949d824a4f3c
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-20 20:44:13 +0000

    Messing with Task XML

--- teacher/task_admin.py
+++ teacher/task_admin.py
@@ -72,7 +72,7 @@ print """Content-type: text/html\n\n
 			function show_XML_info(task_id){
 				get_task_xml(task_id,function(result){
     				var task_info = document.getElementById("task_info");
-    				console.log(result);
+    				console.log(JSON.stringify(JSON.parse(jsonString),null,2);
     				console.log(print_xml_object(result));
     			});
 			}
