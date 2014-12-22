c8f37ca948c2e606c69916845b3c457d7e05cdc2
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-22 18:55:39 +0000

    Moving from Cookies to Sessions overall - header printing fix

--- task_page.py
+++ task_page.py
@@ -3,7 +3,7 @@
 import cgi, cgitb, json, MySQLdb, db_connection,Cookie, common_components,os,datetime
 cgitb.enable()
 
-print "Content-type: html/text\n\n"
+print "
 cookies = Cookie.SimpleCookie(os.environ.get("HTTP_COOKIE",""))
 if cookies.has_key('id') and cookies.has_key('type'):
 	if cookies['type'] == 'Teacher':
@@ -34,7 +34,7 @@ try:
 except MySQLdb.Error, e:
 	print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
 
-print """\n\n
+print """Content-type: text/html\n\n
 
 <html>
 	<head>
