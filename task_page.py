a8c71fa0f86c0c1145c8a353f02e45d1dfe3c7d1
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-23 17:08:45 +0000

    Refactored alot of the task delivery, adding the functionality for testcases [Debugging]

--- task_page.py
+++ task_page.py
@@ -83,7 +83,7 @@ if('testcase' in task_xml and 'method' in task_xml):
 	print """\n<table>
 	<button class="form-control" 
 	onclick='compile_code(document.getElementById("code").value"""+task_id+""","output","error")'
-     type="button">Run</button></div></div>"""
+     type="button">Run</button></div></div></div>"""
 else:
 	print """\n
 		<button class="form-control"
