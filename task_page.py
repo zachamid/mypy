af15080688d3457e65e6429b25bde27ee9bed2ee
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-23 17:03:21 +0000

    Refactored alot of the task delivery, adding the functionality for testcases [Debugging]

--- task_page.py
+++ task_page.py
@@ -87,7 +87,7 @@ else:
 		<button class="form-control"
 		onclick='run_code(document.getElementById("code").value,"output","error")' type="button">
 						Run
-					</button></div>"""
+					</button></div></div>"""
 print """\n		</div>
 			</div>
 		</div>
