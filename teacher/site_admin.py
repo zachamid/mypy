e32cd555f89d9a58b647d430ed7c160156c5c71b
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-25 11:12:10 +0000

    Fix to student table on site_admin

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -76,7 +76,7 @@ print """\n\n
     						row.insertCell(2).innerHTML = result[student]['LastName'];
     						row.insertCell(3).innerHTML = result[student]['Email'];
     						row.insertCell(4).innerHTML = '<a onclick="removeFromClass('+result[student]['StudentID']+')">Remove from class</a>';
-    						row.insertCell(5).innerHTML = "<a onclick=delete('\""+result[student]['StudentID']+"\",\"Student\")'>Delete</a>';
+    						row.insertCell(5).innerHTML = "<a onclick='delete("+result[student]['StudentID']+",'Student')'>Delete</a>";
     						counter++;
     					}
     				});
