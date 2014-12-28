ada9ea57f5c919189341a6966e36bce10d156f5a
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-28 09:46:48 +0000

    site_admin.py - delete student fix

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -76,7 +76,7 @@ print """\n\n
     						row.insertCell(2).innerHTML = result[student]['LastName'];
     						row.insertCell(3).innerHTML = result[student]['Email'];
     						row.insertCell(4).innerHTML = '<a onclick="removeFromClass('+result[student]['StudentID']+')">Remove from class</a>';
-    						row.insertCell(5).innerHTML = "<a onclick='delete_from_table("+result[student]['StudentID']+",'Student')'>Delete</a>";
+    						row.insertCell(5).innerHTML = "<a onclick='delete_from_table("+result[student]['StudentID']+","Student")'>Delete</a>";
     						counter++;
     					}
     				});
@@ -108,9 +108,9 @@ print """\n\n
     						row.insertCell(1).innerHTML = result[student]['FirstName'];
     						row.insertCell(2).innerHTML = result[student]['LastName'];
     						row.insertCell(3).innerHTML = result[student]['Email'];
-    						row.insertCell(4).innerHTML = "<select id='classSelect'"+counter+"'></select>";
-    						row.insertCell(5).innerHTML = "<a onclick='delete_from_table("+result[student]['StudentID']+",'Student')'>Delete</a>";
-    						getClasses('classSelect'+counter);
+    						row.insertCell(4).innerHTML = "<select id='classSelect"+result[student]['StudentID']+"'></select>";
+    						row.insertCell(5).innerHTML = "<a onclick='delete_from_table("+result[student]['StudentID']+","Student")'>Delete</a>";
+    						getClasses('classSelect'+result[student]['StudentID']);
     						counter++;
     					}
     				});
@@ -133,8 +133,7 @@ print """\n\n
       				data : data,
       				url : '/delete.py',
       				type : "POST",
-      				dataType : "text"}).done(function(result){
-      				});
+      				dataType : "text"}).done(function(result){});
  			}
  			
  			function removeFromClass(studentID){
