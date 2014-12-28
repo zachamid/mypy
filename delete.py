9b3f37e444a5cd3fd2ce61510b46eba90acec05a
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-28 10:26:47 +0000

    site_admin.py - delete student fix

--- delete.py
+++ delete.py
@@ -7,5 +7,6 @@ posted_data = cgi.FieldStorage()
 table = posted_data['table'].value
 id = posted_data['id'].value
 sql_query = "DELETE FROM "+table+" WHERE "+table+"ID="+id
+print sql_query
 cursor = db_connection.get_connection()
 cursor.execute(sql_query)
