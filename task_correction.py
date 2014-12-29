c2afa6d94721363773a36d96dd4b8fff4e38136a
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-29 21:10:41 +0000

    Testing correction for the first time - corrected judge functions

--- task_correction.py
+++ task_correction.py
@@ -13,12 +13,12 @@ import task_delivery
 def judge_correctness(id, code):
 	exec(code)
 	py = task_delivery.get_python_code_from_file(id, 'task_complete.py')
-	exec(py['code'])
+	exec(py['task_complete.py'])
 
 def judge_similarity(id, code):
 	print ast.parse(code)
 	py = task_delivery.get_python_code_from_file(id, 'task_complete.py')
-	print ast.parse(py['code'])
+	print ast.parse(py['task_complete.py'])
 
 def judge_time(id,code):
 	print 'Currently developing time metric algorithm'
