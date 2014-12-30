38fb3c2adb326c67d5e7496b98cf4a1944af2d87
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2014-12-30 09:25:02 +0000

    Implementing Levenshtein algorithm for similarity of output

--- task_correction.py
+++ task_correction.py
@@ -10,12 +10,28 @@ import db_connection
 import xml
 import task_delivery
 
+def levenshteinDistance(str1,str2,len1,len2):
+	if len1 == 0:
+		return len2
+	if len2 == 0:
+		return len1
+	
+	cost = 1
+	if str1[len1-1]==str2[len2-1]:
+		cost = 0
+	
+	return min(levenshteinDistance(str1,str2,len1-1,len2)+1,
+	levenshteinDistance(str1,str2,len1,len2-1)+1,
+	levenshteinDistance(str1,str2,len1-1,len2-1)+cost)
+
 def judge_correctness(id, code):
-	exec(code)
+	user_code = eval(code)
 	print '</br>'
 	py = task_delivery.get_python_code_from_file(id, 'task_complete.py')
-	exec(py['task_complete.py'])
+	set_code = eval(py['task_complete.py'])
 	print '</br>'
+	print 'Levenshtein Distance: ', 
+	str(levenshteinDistance(user_code,set_code, len(user_code), len(set_code))
 
 def judge_similarity(id, code):
 	print ast.dump(ast.parse(code))
