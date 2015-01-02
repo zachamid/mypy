8d883e49fe32839da3bce9f3c6eabe307b0f8fe5
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-01-02 21:20:27 +0000

    Testing AST Similarity algorithm Mach2 - printing all of the values in iter_field

--- task_correction.py
+++ task_correction.py
@@ -31,10 +31,10 @@ def ast_visit(node, level=0):
 
 
 def ast_similarity(node1, node2, level=0):
-	for field,val in node1.iter_fields(node1):
+	for field,val in ast.iter_fields(node1):
 		print field+'='+val+';'
 	print '<br>'
-	for field,val in node2.iter_fields(node1):
+	for field,val in ast.iter_fields(node1):
 		print field+'='+val+';'
 
 def levenshteinDistance(str1,str2,len1,len2):
