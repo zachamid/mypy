82c0b60faa542b8eb7dfef1a3775b262022823e4
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-01-02 23:08:23 +0000

    Testing AST Similarity algorithm Mach3 - attempting to convert ast to dict

--- task_correction.py
+++ task_correction.py
@@ -41,10 +41,10 @@ def ast2dict(node):
 			for item in fields[field]:
 				if isinstance(item, ast.AST):
 					print 'encountered AST,'
-					code[item.__class__.__name__] = ast2dict(item)
+					code[item.__name__] = ast2dict(item)
 		if isinstance(fields[field], ast.AST):
 			print 'encountered AST,'
-			code[fields[field].__class__.__name__] = ast2dict(fields[field])
+			code[fields[field].__name__] = ast2dict(fields[field])
 		if isinstance(fields[field], basestring) or isinstance(fields[field],int):
 			code[field] = fields[field]
 	return code
