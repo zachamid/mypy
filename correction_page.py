d77e14faa62320ae0d0dda0a1f5b1753725dfd48
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-01-10 00:36:56 +0000

    Change to correction page - attempting to print dictionaries

--- correction_page.py
+++ correction_page.py
@@ -64,7 +64,7 @@ print """&nbsp				</textarea></div></div>
 						</td>
 						<td>
 """
-#print task_correction.judge_correctness(task_id,student_id,codetocorrect)
+print task_correction.judge_correctness(task_id,student_id,codetocorrect)
 print """&nbsp 			</td>
 					</tr>
 					<tr>
