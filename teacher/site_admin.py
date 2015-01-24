1110e585a67a08aeb89f8e407db9acb7ad10861d
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-01-24 14:58:21 +0000

    TeacherList fucntionality - should now be presentable to admin

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -64,16 +64,16 @@ print """\n\n
  							var currentRow = teacherList.insertRow(row_counter);
  							row_counter++;
  							var currClass = result[0]['ClassID'];
- 							currentRow.insertCell(0).innerHTML='<b>'+result[0][ClassName]+'</b>';
+ 							currentRow.insertCell(0).innerHTML='<b>'+result[0]['ClassName']+'</b>';
  							var currentCell = currentRow.insertCell(1);
 	 						for(counter = 1; counter<result.length; counter++){
-	 							if(currClass != result[counter][ClassID]){
+	 							if(currClass != result[counter]['ClassID']){
 	 								currentRow=teacherList.insertRow(row_counter);
-	 								currentRow.insertCell(0).innerHTML='<b>'+result[counter][ClassName]+'</b>';
+	 								currentRow.insertCell(0).innerHTML='<b>'+result[counter]['ClassName']+'</b>';
 	 								currentCell=currentRow.insertCell(1);
 	 								row_counter++;
 	 							}
-	 							currentCell+=result[counter][FirstName]+' '+result[counter][LastName]+'</br>';
+	 							currentCell+=result[counter]['FirstName']+' '+result[counter]['LastName']+'</br>';
 	 						}
 	 					}
  				});
