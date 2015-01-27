21e17b0cfe315a1638a24861723fb90875576b1b
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-01-27 16:54:23 +0000

    security shaz

---
+++ insert_user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import cgi, cgitb, json, MySQLdb, MySQLdb.cursors, db_connection, hashlib
+cgitb.enable()
+
+posted_data = cgi.FieldStorage()
+table = posted_data['table'].value
+cursor = db_connection.get_connection()
+password = 
