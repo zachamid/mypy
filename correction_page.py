ecf936cdc0e0e4660f4bdf9ae941860673ffaad1
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-03 01:18:25 +0000

    LCS - shaz for correction

--- correction_page.py
+++ correction_page.py
@@ -84,10 +84,7 @@ print """&nbsp 			</td>
 #print task_correction.judge_similarity(task_id, codetocorrect)
 print "</br></br>"
 correctcode = task_delivery.get_python_code_from_file(task_id, 'task_complete.py')['task_complete.py'].split('\n')
-print code
-print "</br></br>"
-print correctcode.split('\n')
-print "</br></br>"
+codetocorrect = codetocorrect.split('\n')
 task_correction.printDiff(task_correction.longest_common_subsequence(codetocorrect,correctcode), codetocorrect, correctcode, len(codetocorrect), len(correctcode))
 print """&nbsp				</td>
 					</tr>
