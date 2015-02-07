0fad4429c0e2e2557bc2adeab96fa53a15551d4b
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-07 22:33:45 +0000

    fix in similarity measure

--- unit_test_files/code3.py
+++ unit_test_files/code3.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-def check_multiples(number, base):
-	if number % base == 0:
-		return 1
-	else:
-		return 0
-		
 for x in xrange(1,10):
-	mult = dict()
-	for y in (2,3,5):
-		mult[str(y)] = check_multiples(x,y)
-	print '%d:%d:%d' % (mult['2'],mult['3'],mult['5'])
+	val2 = 0
+	val3 = 0
+	val5 = 0
+	if x % 2 == 0:
+		val2 = 1
+	if x % 3 == 0:
+		val3 = 1
+	if x % 5 == 0:
+		val5 = 1
+	print '%d:%d:%d' % (val2,val3,val5)
