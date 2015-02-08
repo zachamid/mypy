18ffae30919c0710e8126f2bf4e561c118f01cfb
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-08 22:04:09 +0000

    implementing saving

--- task_delivery.py
+++ task_delivery.py
@@ -107,6 +107,6 @@ def get_python_output(file_path):
 def save_code(task_id, student_id, code):
 	cursor = db_connection.get_connection()
 	curr_date = datetime.datetime.now()	
-	sql = "UPDATE Progress SET Code='%s', DateModified='%s' WHERE TaskID=%s AND StudentID=%s" % (code.replace("'","''"),str(curr_date),str(task_id), str(student_id))
-	cursor.execute(sql)
+	print "UPDATE Progress SET Code='%s', DateModified='%s' WHERE TaskID=%s AND StudentID=%s" % (code.replace("'","''"),str(curr_date),str(task_id), str(student_id))
+	#cursor.execute(sql)
 	
