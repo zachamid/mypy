1727f27149764a31bbcbeffac3bb1e2ff467ba7e
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-08 23:20:27 +0000

    implementing saving

--- correction_page.py
+++ correction_page.py
@@ -83,12 +83,12 @@ print """&nbsp 			</td>
 						</td>
 						<td>"""
 #print task_correction.judge_similarity(task_id, codetocorrect)
-print "</br></br>"
+print "<pre>"
 correctcode = task_delivery.get_python_code_from_file(task_id, 'task_complete.py')['task_complete.py']
-correctcode = correctcode.replace('\t','&nbsp;&nbsp;&nbsp;&nbsp;').split('\n')
-codetocorrect = code.replace('\t','&nbsp;&nbsp;&nbsp;&nbsp;').split('\n')
+correctcode = correctcode.split('\n')
+codetocorrect = code.split('\n')
 task_correction.printDiff(task_correction.longest_common_subsequence(codetocorrect,correctcode), codetocorrect, correctcode, len(codetocorrect), len(correctcode))
-print """&nbsp				</td>
+print """\n				</pre></td>
 					</tr>
 					<tr>
 						<td>
