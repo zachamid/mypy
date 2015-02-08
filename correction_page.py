078f8046cc5fe95ab81a1eb84644ea93c16c703b
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-08 23:16:21 +0000

    implementing saving

--- correction_page.py
+++ correction_page.py
@@ -86,7 +86,7 @@ print """&nbsp 			</td>
 print "</br></br>"
 correctcode = task_delivery.get_python_code_from_file(task_id, 'task_complete.py')['task_complete.py'].split('\n')
 codetocorrect = code.split('\n')
-task_correction.printDiff(task_correction.longest_common_subsequence(codetocorrect,correctcode), codetocorrect, correctcode, len(codetocorrect), len(correctcode))
+task_correction.printDiff(task_correction.longest_common_subsequence(correctcode,codetocorrect), correctcode, codetocorrect, len(correctcode), len(codetocorrect))
 print """&nbsp				</td>
 					</tr>
 					<tr>
