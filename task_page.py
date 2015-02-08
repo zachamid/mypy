4aabaec744574a44196ab1fd36caa28d7da8eaa5
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-08 22:53:58 +0000

    implementing saving

--- task_page.py
+++ task_page.py
@@ -67,7 +67,9 @@ print """Content-type: text/html\n\n
     		mapForm.appendChild(output);
     		mapForm.appendChild(code);
     		document.getElementById('postform').appendChild(mapForm);
-    		mapForm.submit();
+    		console.log(code.value);
+    		console.log(output.value);
+    		//mapForm.submit();
 		}
 		$(function() {
 			code_area_prep();
