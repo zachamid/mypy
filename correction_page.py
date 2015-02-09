5fc366aee4b0e75234b9cfdbc9e1026491ff56af
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-09 10:03:56 +0000

    CORRECTION: Jaccard implementation

--- correction_page.py
+++ correction_page.py
@@ -86,10 +86,13 @@ print """&nbsp 			</td>
 #print task_correction.judge_similarity(task_id, codetocorrect)
 print "<pre>"
 correctcode = task_delivery.get_python_code_from_file(task_id, 'task_complete.py')['task_complete.py']
+jaccard_score = task_correction.compare_asts(correctedcode, codetocorrect)
 correctcode = re.sub(' +','  ',correctcode).split('\n')
 codetocorrect = re.sub(' +','  ',codetocorrect).split('\n')
 task_correction.printDiff(task_correction.longest_common_subsequence(codetocorrect,correctcode), codetocorrect, correctcode, len(codetocorrect), len(correctcode))
-print """\n				</pre></td>
+print "				</pre></br>"
+print """\n
+						</td>
 					</tr>
 					<tr>
 						<td>
