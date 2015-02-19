dc18b6b2d8c58069dfdac84e53cf265838ad591d
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-19 00:15:03 +0000

    Task Creator - Create Task Function

--- teacher/create_task.py
+++ teacher/create_task.py
@@ -20,19 +20,19 @@ id = cursor.lastrowid
 
 path = '/var/www/tasks/'+str(id)
 os.mkdir(path)
-task_complete_py = open(path+'/task_complete.py','rw')
+task_complete_py = open(path+'/task_complete.py','w')
 task_complete_py.write(task_complete)
 task_complete_py.close()
 
-task_skeleton_py = open(path+'/task_skeleton.py','rw')
+task_skeleton_py = open(path+'/task_skeleton.py','w')
 task_skeleton_py.write(task_skeleton)
 task_skeleton_py.close()
 
-result_txt = open(path+'/result.txt','rw')
+result_txt = open(path+'/result.txt','w')
 result_txt.write(result)
 result_txt.close()
 
-xml_file = open(path+'/info.xml','rw')
+xml_file = open(path+'/info.xml','w')
 task_info = json.loads(task_json).value
 task_xml = dicttoxml.dicttoxml(task_info)
 xml_file.write(task_xml)
