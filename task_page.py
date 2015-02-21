d7ce6bd7f70f5ee4b22017df7f1da81472fb41ee
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-21 15:05:58 +0000

    Adding CodeMirror to task_page

--- task_page.py
+++ task_page.py
@@ -130,7 +130,7 @@ if new_flag == 0:
 else:
 	code = task_delivery.get_python_code_from_file(task_id, 'task_skeleton.py')['task_skeleton.py']
 	cursor.execute("""INSERT INTO Progress (StudentID, TaskID, DateStarted, Code)
-						Values(%s, %s, %s, %s)""" % (str(student_id),str(task_id),"'"+str(curr_date)+"'","'"+code+"'"))
+						Values('%s', '%s', '%s', '%s')""" % (str(student_id),str(task_id),str(curr_date),code))
 	print code
 
 print """\n				</textarea>
