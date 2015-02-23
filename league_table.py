1750ec81915bb8aa6020436be183887c07ebf8ac
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-23 22:10:23 +0000

    League Table fix

--- league_table.py
+++ league_table.py
@@ -52,7 +52,7 @@ print """\n
     	<link rel="stylesheet" type="text/css" href="general_style.css">
     	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
     	<style>
-    		td,tr {
+    		td {
  				padding:15px;
  				vertical-align: top;
 			}
