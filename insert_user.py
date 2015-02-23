907a3d7efc861466f527166d54d832a3af01696b
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-23 20:39:43 +0000

    Sign Up fix

--- insert_user.py
+++ insert_user.py
@@ -10,7 +10,6 @@ password = hashlib.sha256(posted_data['Password'].value).hexdigest()
 email = posted_data['Email'].value
 fName = posted_data['FirstName'].value
 lName = posted_data['LastName'].value
-table = posted_data['table'].value
 sql_query = '''INSERT INTO %s (FirstName, LastName, Email, Password) VALUES (%s,%s,%s,%s)''' % (table,fName,lName,email,password)
 cursor.execute(sql_query)
 id = cursor.lastrowid
