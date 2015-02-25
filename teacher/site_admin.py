016de378afdc8b1f498f389d2cdef8f33b838e28
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-02-25 16:11:12 +0000

    Site Admin style issues

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -318,8 +318,8 @@ print """\n
 		</div>
 		<div class="container col-sm-12 col-md-12 ">
 			<div class="panel panel-default translucent" style='width:100%;display: inline-block'>
-			<div class="panel panel-default col-sm-12 col-md-6 translucent">
-				<div class="panel-body">
+			<div class="panel panel-default col-sm-12 col-md-6">
+				<div class="panel-body translucent">
 				<h4>Teacher Class Assignments </h4></br>
 				<select onclick='getTeacherList(this.value)' class="form-control" id='class_select'></select>
 				<table id='teacherList'></table>
@@ -330,8 +330,8 @@ print """\n
 				</table>
 				</div>
 			</div>
-			<div class="panel panel-default col-sm-12 col-md-6 translucent">
-				<div class="panel-body">
+			<div class="panel panel-default col-sm-12 col-md-6">
+				<div class="panel-body translucent">
 				<h4>Administrators</h4></br>
 				<table id='admin_table'></table>
 				</br>
