a7aae615a1c29aab2eb3db41c3e6754b7abe53ac
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-01 19:57:21 +0000

    SiteAdmin fix

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -194,7 +194,7 @@ print """\n\n
     						row.insertCell(1).innerHTML = result[student]['FirstName']+' '+result[student]['LastName'];
     						row.insertCell(2).innerHTML = result[student]['Email'];
     						row.insertCell(3).innerHTML = "<select class='form-control' id='classSelect"+result[student]['StudentID']+"'></select>";
-    						row.insertCell(3).innerHTML += "<button class='form-control' onclick='AssignStudentToClass("+result[student]['StudentID']+",document.getElementById('classSelect"+result[student]['StudentID']+"').value)'>Assign</button>"
+    						row.insertCell(3).innerHTML += "<button class='form-control' onclick=\"AssignStudentToClass("+result[student]['StudentID']+",document.getElementById('classSelect"+result[student]['StudentID']+"').value)\">Assign</button>"
     						row.insertCell(4).innerHTML = "<button  class='form-control' onclick='deleteStudent("+result[student]['StudentID']+")'>Delete</button>";
     						getClasses('classSelect'+result[student]['StudentID']);
     						counter++;
