46af5f29b0264cf83ec578bda98534a9d23aaa62
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-01 15:12:14 +0000

    Mako Intro

--- mako_play.py
+++ mako_play.py
@@ -1,17 +1,2 @@
-<%inherit file="base.html"/>
-<%
-    rows = [[v for v in range(0,10)] for row in range(0,10)]
-%>
-<table>
-    % for row in rows:
-        ${makerow(row)}
-    % endfor
-</table>
-
-<%def name="makerow(row)">
-    <tr>
-    % for name in row:
-        <td>${name}</td>\
-    % endfor
-    </tr>
-</%def>
+from mako.template import Template
+print(Template("hello ${data}!").render(data="world"))
