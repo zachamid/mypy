84901d52dd5c2daee31a6f1efed9b1a97f4ac7a5
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-03 13:44:51 +0000

    Mako refactorisation on the League Table Page

--- league_table.py
+++ league_table.py
@@ -16,28 +16,33 @@ if cookies.has_key('id') and cookies.has_key('type'):
 else:
 	html_header += 'Location: index.py'
 
-cursor = db_connection.get_connection()
-cursor.execute('''SELECT FirstName, LastName, ClassID FROM Student WHERE StudentID=%s''' % (cookies['id'].value))
-student_record = cursor.fetchone()
-cursor.execute('''SELECT StudentID, FirstName, LastName FROM Student
-					WHERE ClassID=%s''' % (student_record['ClassID']))
-class_list = cursor.fetchall()
 league_entry = []
-for student in class_list:
-	new_entry = {}
-	new_entry['student'] = str(student['StudentID'])
-	new_entry['total_score'] = 0
-	cursor.execute('''SELECT Correctness_Points,Similarity_Points, Attempts_Points, Time_Points 
-						FROM Progress WHERE StudentID='''+str(student['StudentID']))
-	progress_records = cursor.fetchall()
-	for record in progress_records:
-		score = calc_score(record['Correctness_Points'],record['Similarity_Points'],record['Attempts_Points'],record['Time_Points'])
-		new_entry['total_score'] += score
-	new_entry['no_tasks'] = len(progress_records)
-	new_entry['name']= student['FirstName']+' '+student['LastName']
-	league_entry.append(new_entry)
+name = ''
+league_order = []
+if cookies['type'].value == 'Student':
+	cursor = db_connection.get_connection()
+	cursor.execute('''SELECT FirstName, LastName, ClassID FROM Student WHERE StudentID=%s''' % (cookies['id'].value))
+	student_record = cursor.fetchone()
+	name = student_record['FirstName'] + ' ' + student_record['LastName']
+	cursor.execute('''SELECT StudentID, FirstName, LastName FROM Student
+					WHERE ClassID=%s''' % (student_record['ClassID']))
+	class_list = cursor.fetchall()
+
+	for student in class_list:
+		new_entry = {}
+		new_entry['student'] = str(student['StudentID'])
+		new_entry['total_score'] = 0
+		cursor.execute('''SELECT Correctness_Points,Similarity_Points, Attempts_Points, Time_Points 
+							FROM Progress WHERE StudentID='''+str(student['StudentID']))
+		progress_records = cursor.fetchall()
+		for record in progress_records:
+			score = calc_score(record['Correctness_Points'],record['Similarity_Points'],record['Attempts_Points'],record['Time_Points'])
+			new_entry['total_score'] += score
+		new_entry['no_tasks'] = len(progress_records)
+		new_entry['name']= student['FirstName']+' '+student['LastName']
+		league_entry.append(new_entry)
 		
-league_order=sorted(league_entry, key=lambda e: e['total_score'], reverse=True)
+	league_order=sorted(league_entry, key=lambda e: e['total_score'], reverse=True)
 
 include_lookup = TemplateLookup(directories=[os.getcwd()])
 template_file = open('league_table_template.html','r')
