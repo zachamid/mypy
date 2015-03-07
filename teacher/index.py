254aba666f54ffc6154b6d56df128829aa8c366d
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-07 12:18:39 +0000

    Moving everything to Mako from Student

--- teacher/index.py
+++ teacher/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
 import Cookie, cgi, cgitb, os,sys
-sys.path.append(os.pardir)
-import common_components, db_connection
 from mako.template import Template
 from mako.lookup import TemplateLookup
+sys.path.append(os.pardir)
+import common_components, db_connection
 
 cgitb.enable()
 
