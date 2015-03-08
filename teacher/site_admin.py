4b4fdfd737b763541e3793b92a7866c10ee2e1ba
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-08 20:24:19 +0000

    Fixing visibility issues - teacher/site_admin.py

--- teacher/site_admin.py
+++ teacher/site_admin.py
@@ -14,15 +14,16 @@ html_header = ''
 name = ''
 if cookies.has_key('id') and cookies.has_key('type'):
 	html_header += str(cookies)
-	if cookies['type'].value == 'Teacher':
+	if cookies['type'].value == 'Student':
+		html_header += 'Location:../index.py'
+	else:
 		cursor.execute('SELECT * FROM Teacher WHERE TeacherID='+cookies['id'].value)
 		record = cursor.fetchone()
 		if record['Administrator'] == 0:
 			html_header += 'Location:index.py'
 		else:
 			name = record['FirstName'] + ' ' + record['LastName']
-	else:
-		html_header += 'Location:../index.py'
+		
 else:
 	html_header += 'Location:../index.py'
 
