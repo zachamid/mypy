ed0211bdabea8e98961df5a335a158a874cfcb61
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-08 19:47:33 +0000

    First Attempt at automated testing

--- unit_test_files/visibility.py
+++ unit_test_files/visibility.py
@@ -4,7 +4,21 @@ from splinter import Browser
 
 browser = Browser('zope.testbrowser')
 url = 'http://webvm.cs.man.ac.uk/'
-sites = ['correction_page.py','league_table.py','playground.py','sign_up.py','task_list.py','task_page.py','user_page.py']
+sites = [
+	'correction_page.py',
+	'league_table.py',
+	'playground.py',
+	'sign_up.py',
+	'task_list.py',
+	'task_page.py',
+	'user_page.py',
+	'/teacher/class_results.py',
+	'/teacher/index.py',
+	'/teacher/sign_up.py',
+	'/teacher/site_admin.py',
+	'/teacher/task_admin.py',
+	'/teacher/task_creator.py'
+]
 
 for site in sites:
 	browser.visit(url+site)
