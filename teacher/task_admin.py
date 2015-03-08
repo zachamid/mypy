023b52ba283d22c08225be789642ec3cf92a8d14
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-08 20:26:29 +0000

    Fixing visibility issues - teacher/task_admin.py

--- teacher/task_admin.py
+++ teacher/task_admin.py
@@ -18,15 +18,15 @@ cursor = db_connection.get_connection()
 if cookies.has_key('id') and cookies.has_key('type'):
 	html_header += str(cookies)
 	if cookies['type'].value == 'Teacher':
-		html_header += 'Location:index.py'
+		html_header += 'Location: ../index.py'
+	else:
 		cursor.execute('SELECT * FROM Teacher WHERE TeacherID='+cookies['id'].value)
 		record = cursor.fetchone()
 		type = 'Teacher'
 		if record['Administrator'] == 0:
 			type = 'Administrator'
 		name = record['FirstName'] + ' ' + record['LastName']
-	else:
-		html_header += 'Location: ../index.py'
+		
 else:
 	html_header += 'Location: ../index.py'
 
