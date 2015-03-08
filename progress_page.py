e08814c3f52a4b3457e3ccdba559c219890eaf7f
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-08 17:16:45 +0000

    Mako change for progress page

--- progress_page.py
+++ progress_page.py
@@ -4,87 +4,31 @@ import cgi, cgitb, json, MySQLdb, db_connection, Cookie, common_components,os
 from task_correction import calc_score as calc_score
 cgitb.enable()
 
+html_header = ''
+name = ''
+progress_records = {}
 cookies = Cookie.SimpleCookie(os.environ.get("HTTP_COOKIE",""))
 if cookies.has_key('id') and cookies.has_key('type'):
-	print cookies
-	if cookies['type'].value == 'Teacher':
-		print 'Location: index.py'
-else:
-	print 'Location: index.py'
-
-cursor = db_connection.get_connection()
-cursor.execute('''SELECT * FROM Progress
+	html_header += str(cookies)
+	cursor = db_connection.get_connection()
+	cursor.execute('SELECT FirstName, LastName FROM Student WHERE StudentID='+cookies['id'].value)
+	names = cursor.fetchone()
+	name = names['FirstName']+' '+names['LastName']
+	cursor.execute('''SELECT * FROM Progress
 					INNER JOIN Task
 					ON Progress.TaskID = Task.TaskID
-					WHERE Progress.StudentID=%s''',(cookies['id'].value))
-progress_records = cursor.fetchall()
-
-print """Content-type: text/html\n\n
-
-<html>
-	<head>
-		<script src="skulpt-latest/skulpt.min.js" type="text/javascript"></script> 
-		<script src="skulpt-latest/skulpt-stdlib.js" type="text/javascript"></script> 
-		<script src="jquery-1.11.1.min.js" type="text/javascript"></script> 
-		<script src="python_functions.js" type="text/javascript"></script>
-		<script src="user_functions.js" type="text/javascript"></script>
-		<script src="task_admin_functions.js" type="text/javascript"></script>
-    	<link rel="stylesheet" type="text/css" href="general_style.css">
-    	<link rel="stylesheet" type="text/css" href="bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
-		<script>
-			function open_correction(taskID){
-				var mapForm = document.createElement("form");
-	    		mapForm.method = "POST";
-	  		  	mapForm.action = "/correction_page.py";
-				var taskid = document.createElement("input");
-			    taskid.type = "text";
-			    taskid.name = "task_id";
-				taskid.value = taskID;
-			    mapForm.appendChild(taskid);
-    			document.getElementById('postform').appendChild(mapForm);
-    			mapForm.submit();
-			}
-		</script>
-		<style>
-			table,td,th, tr {
-    			border: 0.5px solid black;
-    			padding: 10px;
-			}
-		</style>
-	</head>
-	<body>"""
-common_components.print_navbar(cookies['id'].value,'')
-print """\n
-		<div class="container col-sm-6 col-md-9">
-			<div class="panel  panel-default translucent">
-				<table width="100%">
-				<tr>
-					<td><b>ID</b></td>
-					<td><b>Task Name</b></td>
-					<td><b>Date Started</b></td>
-					<td><b>Date Modified</b></td>
-					<td><b>Date Completed</b></td>
-					<td><b>Score</b></td>
-				</tr>"""
-for record in progress_records:
-	print '<tr onclick=\'open_correction('+str(record['TaskID'])+')\'><td>'+str(record['TaskID'])+'</td>'
-	print '<td>'+record['Title']+'</td>'
-	print '<td>'+record['DateStarted'].strftime("%Y-%m-%d %H:%M:%S")+'</td>'
-	if record['DateModified'] is not None:
-		print '<td>'+record['DateModified'].strftime("%Y-%m-%d %H:%M:%S")+'</td>'
-	else:
-		print '<td></td>'
-	if record['DateCompleted'] is not None:
-		print '<td>'+record['DateCompleted'].strftime("%Y-%m-%d %H:%M:%S")+'</td>'
-		score = calc_score(record['Correctness_Points'],record['Similarity_Points'],record['Time_Points'],record['Attempts_Points'])
-		print '<td>'+str(score)+'</td>'
-	else:
-		print '<td></td>'
-		print '<td></td>'
-	print '</tr>'
-print """\n	</table>
-		</div></div>
-		<div id="postform" style="display: none;"></div>
-	</body>
-</html>
-"""
+					WHERE Progress.StudentID=%s'''%(cookies['id'].value))
+	progress_records = cursor.fetchall()
+	for progress in progress_records:
+		progress['score'] = calc_score(progress['Correctness_Points'],progress['Similarity_Points'],progress['Attempts_Points'],progress['Time_Points'])
+	
+	if cookies['type'].value == 'Teacher':
+		html_header += 'Location: index.py'
+else:
+	html_header += 'Location: index.py'
+	
+include_lookup = TemplateLookup(directories=[os.getcwd()])
+template_file = open('progress_page_template.html','r')
+template = template_file.read()
+page_template = Template(template, lookup=include_lookup)
+print page_template.render(html_header=html_header, name=name, progress_records=progress_records)
