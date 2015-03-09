cbe06526e960e0b7a2181c4b048da49060b022ab
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-09 20:41:07 +0000

    Task Page - new posting system - Debugging

--- correction_page.py
+++ correction_page.py
@@ -62,10 +62,7 @@ if cookies.has_key('id') and cookies.has_key('type') and task_info.has_key('task
 		
 				jaccard_score = task_correction.compare_asts(correct_code, submitted_code)
 			
-				if progress_record is None:
-					attempt_score = task_correction.judge_attempts(1)
-				else:
-					attempt_score = task_correction.judge_attempts(progress_record['Attempts']+1)
+				attempt_score = task_correction.judge_attempts(progress_record['Attempts']+1)
 	
 				min_time = task_correction.quickest_time(times)
 				task_time = (progress_record['DateCompleted']-progress_record['DateStarted']).seconds
