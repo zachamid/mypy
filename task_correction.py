38c5dcf9bed61a462e0280178453606d49393429
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-15 23:32:36 +0000

    Task Correction fix

--- task_correction.py
+++ task_correction.py
@@ -146,7 +146,7 @@ def remark_time(min_time, times):
 	for time in times:
 		if(time['DateCompleted'] is not None):
 			delta = (time['DateCompleted'] - time['DateStarted']).seconds
-			new_score = (float)(min_time)/delta
+			new_score = judge_time(min_time, delta)
 			cursor.execute('''UPDATE Progress SET Time_Points=%s WHERE ProgressID=%s
 							''' % (str(new_score), str(time['ProgressID'])))
 		
