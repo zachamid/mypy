10cb274753772779792d4608b391e1378f8cbf07
mbax9zh2 <zaccaria.hamid@student.manchester.ac.uk>
2015-03-16 16:21:23 +0000

    Fixing Jaccard

--- example.py
+++ example.py
@@ -1,44 +1,21 @@
-#!/usr/bin/env python
+'''
+Use the fibonacci function to complete the task
+'''
+def fibonacci(n):
+  if( n == 0):
+    return 0
+  else:
+    x = 0
+    y = 1
+    for i in range(1,n):
+      z = (x + y)
+      x = y
+      y = z
+    return y
 
-import session, Cookie, cgi, cgitb, os
-
-cgitb.enable()
-
-string_cookie = os.environ.get('HTTP_COOKIE')
-cookie = session.return_cookie()
-
-# If new session
-if not session.in_session():
-	session.set_session('Student', 0)
-# If already existent session
-else:
-	cookie.load(string_cookie)
-
-id = cookie['id'].value
-type = cookie['type'].value
-session.set_session(type, int(id)+1)
-	
-session.print_cookie()
-print """Content-type: text/html\n\n
-
-<html><head><script src="/jquery-1.11.1.min.js"></script>
-<script>
-function clear_cookies(){
-	var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-    	var cookie = cookies[i];
-    	var eqPos = cookie.indexOf("=");
-    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-    
-    	window.location.reload();
-}
-</script>
-</head>"""
-if session.in_session:
-   print type,'</br>',id
-
-print """\n
-<button onclick="clear_cookies()"> Clear </button>"""
+'''
+The print_fibonacci function is used to print the 
+result for the output
+'''
+def print_fibonacci(n):
+  print fibonacci(n)
